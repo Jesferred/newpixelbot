10939471cecb4559b5e4b7157a5a4a52d9c42d30
JesferMonkaS <44315534+JesferMonkaS@users.noreply.github.com>
2019-10-10 13:54:13 +0300

    Update Pixelbattle Placer.user.js

--- Pixelbattle Placer.user.js
+++ Pixelbattle Placer.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Pixelbattle Placer
+// @name         bot.js
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Unity is the way to victory
